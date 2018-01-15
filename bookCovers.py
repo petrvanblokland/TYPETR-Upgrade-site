64145f890d36b98c33ed3215915b2b69a66a9f4f
petrvanblokland <buro@petr.com>
2018-01-15 16:57:01 +0100

    Adding 2 book covers

--- bookCovers.py
+++ bookCovers.py
@@ -16,93 +16,127 @@
 #
 from pagebot.contexts import defaultContext as context
 from pagebot.fonttoolbox.objects.font import getFontByName
-from pagebot.style import A4, CENTER, DISPLAY_BLOCK, RIGHT, LEFT
 from pagebot import Gradient, Shadow
 from pagebot.toolbox.dating import now
 
-shadow = Shadow(offset=(6, -6), blur=10, color=(0.2, 0.2, 0.2, 0.5))
+W, H = 400, 600
 
-W, H = A4
-
-def buildCoverPages(w, h, year):
-	
-    footNoteRef = 12
-
-    magazineTitle = 'Habit'
-
-    M = 2 # Margin
-    ML, MR, MT, MB = M, 0.75*M, M, 1.5*M
-    cw = w-ML-MR
-    
-    # Page 66
-    context.newPage(w, h) 
-    
-    # Draw image, covering all page, scaled.
-    context.image('docs/images/IMG_0672-50.jpg', (-14, -5), w=w*1.05, h=h*1.05)
-    
-    y = h
-    
-    # Title of cover, make it fit in with and add shadow
-    coverTitleStyle = dict(font='Upgrade-Bold', fontSize=100, textFill=1)
-    bs = context.newString(magazineTitle, style=coverTitleStyle, w=w-4*M)  
-    tw, th = bs.size()  
-    context.setShadow(shadow)
-    context.text(bs, (ML, y-th*0.65))
-    context.resetShadow()
-
-    y -= th
-    # Title of cover, make it fit in with and add shadow
-    style = dict(font='Upgrade-Light', fontSize=100, textFill=0.1)
-    bs = context.newString('Upgrade loveables', style=style, w=w*0.75-18*M)  
-    tw, th = bs.size()  
-    context.text(bs, (ML*14, y+th*0.65))
- 
-     # Title of cover, make it fit in with and add shadow
-    style = dict(font='Upgrade-BlackItalic', fontSize=100, textStroke=1, textStrokeWidth=2, textFill=None)
-    bs = context.newString(year, style=style, w=w/2-24*M)  
-    tw, th = bs.size()  
-    context.text(bs, (w/2, MB+20))
-    
 def buildCoverPages1(w, h, year):
-	
-    footNoteRef = 12
 
-    magazineTitle = u'Habit'
-
-    M = 2 # Margin
-    ML, MR, MT, MB = M, 0.75*M, M, 1.5*M
-    cw = w-ML-MR
+    M = 10
     
     # Page 66
     context.newPage(w, h) 
     
-    # Draw image, covering all page, scaled.
-    context.image('docs/images/IMG_3076-50.jpg', (-14, -5), w=w*1.05, h=h*1.05)
+    context.fill(0.1)
+    context.rect(0, 0, w, h)
     
-    y = h
+    c1 = (0.2, 0.7, 1)
+    c2 = 0.8
+    
+    y = h-M
     
     # Title of cover, make it fit in with and add shadow
-    coverTitleStyle = dict(font='Upgrade-Bold', fontSize=100, textFill=(0.85, 0.85, 1, 0.95))
-    bs = context.newString(magazineTitle, style=coverTitleStyle, w=w-4*M)  
-    tw, th = bs.size()  
-    context.text(bs, (ML, y-th*0.65))
+    coverTitleStyle = dict(font='Upgrade-Hairline', fontSize=100, textFill=c1)
+    bs = context.newString('THE', style=coverTitleStyle, w=w-M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+    
+    y -= bh-by+M/2
 
-    y -= th
+    coverTitleStyle = dict(font='Upgrade-Thin', fontSize=100, textFill=c1)
+    bs = context.newString('LONGEST', style=coverTitleStyle, w=w-1.5*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+    y -= bh-by+M/2
+
     # Title of cover, make it fit in with and add shadow
-    style = dict(font='Upgrade-Light', fontSize=100, textFill=1, rLeading=1)
-    bs = context.newString('Upgrade\nServices', style=style, w=w/3.5)  
-    tw, th = bs.size()  
-    context.text(bs, (w*2/3, y+th/2))
+    coverTitleStyle = dict(font='Upgrade-ExtraLight', fontSize=100, textFill=c1, rTracking=0.05)
+    bs = context.newString('MELLIFLUOUSLY', style=coverTitleStyle, w=w-1.5*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
 
-   # Title of cover, make it fit in with and add shadow
-    coverTitleStyle = dict(font='Upgrade-BlackItalic', fontSize=100, textStroke=(1, 1, 1, 0.6), textStrokeWidth=2.5, textFill=None)
-    bs = context.newString(year, style=coverTitleStyle, w=w/2.5-24*M)  
-    tw, th = bs.size()  
-    context.text(bs, (M*3, MB+20))
+    y -= bh-by+M/2
+
+    coverTitleStyle = dict(font='Upgrade-Light', fontSize=100, textFill=c1, rTracking=0.07)
+    bs = context.newString('supercalifragilisticexpialidociously'.upper(), style=coverTitleStyle, w=w-2*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+    y -= bh-by+M/2
+
+    coverTitleStyle = dict(font='Upgrade-Book', fontSize=100, textFill=c1, rTracking=0.07)
+    bs = context.newString('pneumonoultramicroscopicsilicovolcanoconiosis'.upper(), style=coverTitleStyle, w=w-2*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+    y -= bh-by+M/2
+    
+    coverTitleStyle = dict(font='Upgrade-Light', fontSize=100, textFill=c1)
+    bs = context.newString('INTERMIXED', style=coverTitleStyle, w=w-1.5*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+    y -= bh-by+2*M
+
+    # Title of cover, make it fit in with and add shadow
+    coverTitleStyle = dict(font='Upgrade-Light', fontSize=100, textFill=c2)
+    bs = context.newString('MATTHEW', style=coverTitleStyle, w=w-1.5*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+    y -= bh-by+M/2
+    
+    # Title of cover, make it fit in with and add shadow
+    coverTitleStyle = dict(font='Upgrade-Light', fontSize=100, textFill=c2)
+    bs = context.newString('DOW', style=coverTitleStyle, w=w-M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+def buildCoverPages2(w, h, year):
+    
+    M = 30
+    
+    # Page 66
+    context.newPage(w, h) 
+    
+    context.fill(0.1)
+    context.rect(0, 0, w, h)
+    
+    c1 = (0.2, 0.7, 1)
+    c2 = 0.8
+    
+    y = h-M
+    
+    
+    # Title of cover, make it fit in with and add shadow
+    coverTitleStyle = dict(font='Upgrade-Book', fontSize=100, textFill=1, rTracking=0.2, openTypeFeatures=dict(smcp=True) )
+    bs = context.newString('One Lightyear equals', style=coverTitleStyle, w=w-2*M)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, y-bh+by))
+
+    y -= 100
+    
+    coverTitleStyle = dict(font='Upgrade-Thin', fontSize=100, textFill=c1, rTracking=0.2, rLeading=0.9, openTypeFeatures=dict(tnum=True) )
+    bs = context.newString('9460\n7304\n7258\n0800\n512', style=coverTitleStyle, w=w-M/2)  
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w/2-(bw+bx)/2, -by+1.5*M))
+    
+    coverTitleStyle = dict(font='Upgrade-ExtraLight', fontSize=100, textFill=c1, rTracking=0.05, rLeading=0.9)
+    bs = context.newString('mm', style=coverTitleStyle, w=w/5)    
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w*4/6+M, -by+3.2*M))
+    
+    coverTitleStyle = dict(font='Upgrade-Regular', fontSize=100, textFill=c2, rTracking=0.2, rLeading=1, openTypeFeatures=dict(smcp=True) )
+    bs = context.newString('Matthew\nDow', style=coverTitleStyle, w=w/5)    
+    bx, by, bw, bh = bs.bounds()
+    context.text(bs, (w*4/6+M, -by+1.5*M))
+    
     
 
 IMAGES = (
-    ('docs/documents/bookCoverPages.pdf', W, H, now().year, buildCoverPages),    ('docs/images/bookCoverPages.png', W*3/4, H*3/4, now().year, buildCoverPages),    ('docs/documents/bookCoverPages1.pdf', W, H, now().year+1, buildCoverPages1),    ('docs/images/bookCoverPages1.png', W*3/4, H*3/4, now().year+1, buildCoverPages1),)        
+    ('docs/documents/bookCoverPages1.pdf', W, H, now().year, buildCoverPages1),    ('docs/images/bookCoverPages1.png', W, H, now().year, buildCoverPages1),    ('docs/documents/bookCoverPages2.pdf', W, H, now().year, buildCoverPages2),    ('docs/images/bookCoverPages2.png', W, H, now().year, buildCoverPages2),)        
       
 for path, w, h, year, m in IMAGES:
     newDrawing()
